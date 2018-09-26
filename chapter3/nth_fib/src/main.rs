98c8f144d4ccfc228ee254f95269f15d2fb94f6c
steavot <tomsteavenson@gmail.com>
2018-09-26 19:24:31 +0100

    some defense against large numbers

--- chapter3/nth_fib/src/main.rs
+++ chapter3/nth_fib/src/main.rs
@@ -10,24 +10,29 @@ fn main() {
         io::stdin().read_line(&mut n)
             .expect("Failed to read line");
 
-        let n: u32 = match n.trim().parse() {
+        let n: u128 = match n.trim().parse() {
             Ok(num) => num,
             Err(_) => continue,
         };
 
-        let nth_fib = find_nth_fib(&n);
+        if n > 186 {
+            println!("Too big, give me a smaller n!");
+            continue;
+        }
+
+        let nth_fib = find_nth_fib(n);
 
         println!("{}", &nth_fib);
         break;
     }
 }
 
-fn find_nth_fib(n: & u32) -> u32 {
-    let mut a: u32 = 1;
-    let mut b: u32 = 1;
-    let mut count: u32 = 2;
+fn find_nth_fib(n: u128) -> u128 {
+    let mut a: u128 = 1;
+    let mut b: u128 = 1;
+    let mut count: u128 = 2;
 
-    while count < *n {
+    while count < n {
         let temp = a + b;
         a = b;
         b = temp;
